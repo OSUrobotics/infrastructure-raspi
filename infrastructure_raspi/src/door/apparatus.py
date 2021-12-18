7cf74fcc9baa085f2f01b8a7c8d751e1b1221b43
Ryan Roberts <roberyan@oregonstate.edu>
2021-12-18 14:05:08 -0800

    Create apparatus.py

---
+++ infrastructure_raspi/src/door/apparatus.py
@@ -0,0 +1,7 @@
+# include any source code for the apparatus in this directory
+#
+# ApparatusClass is main class that template_controller interfaces with
+
+class ApparatusClass:
+  def __init__(self):
+    pass
