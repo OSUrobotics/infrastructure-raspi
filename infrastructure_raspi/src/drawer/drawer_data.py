f256d2440e5c76795c1d214031800bc2bf4a4bd3
Ryan Roberts <roberyan@oregonstate.edu>
2021-12-18 14:17:00 -0800

    Delete infrastructure_raspi/src/drawer directory

--- infrastructure_raspi/src/drawer/drawer_data.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-class DataPoint:
-  def __init__(self, tof, handle): #, time):
-    self.tof = tof
-    self.handle_data = handle
-  
-  def __del__(self):
-    del self.handle_data
