8680ffe6a153814b036e852a65df76d47f3cb5c2
Grimmlins <osu.grimmlins@gmail.com>
2022-05-03 22:06:17 +0000

    adding new stepper motor control

--- infrastructure_raspi/src/testbed_controller.py
+++ infrastructure_raspi/src/testbed_controller.py
@@ -24,33 +24,39 @@ class HardwareController():
     def __init__(self, hardware):
         #action servers
         self.hardware = hardware
-	self.parameters_as = actionlib.SimpleActionServer("set_test_parameters", TestParametersAction, self.parameter_callback, False) 
-	self.parameters_as.start()
-	self.reset_as = actionlib.SimpleActionServer("reset_hardware", StageAction, self.reset_callback, False) 
-	self.reset_as.start()
+        self.parameters_as = actionlib.SimpleActionServer("set_test_parameters", TestParametersAction, self.parameter_callback, False) 
+        self.parameters_as.start()
+        self.reset_as = actionlib.SimpleActionServer("reset_hardware", StageAction, self.reset_callback, False) 
+        self.reset_as.start()
 
     def parameter_callback(self, goal):
-        reset_angle = goal.parameters[1]
         trial_object = goal.parameters[0]
+        reset_angle = goal.parameters[1]
+        
         try:
+            print("set_test_parameters")
+            print("trial_object: " + str(trial_object))
+            print("reset_angle: " + str(reset_angle))            
             self.parameters_as.publish_feedback(TestParametersFeedback(status="setting object to: {}".format(trial_object)))
             #add testbed start trial call here
-	    self.hardware.action_caller(trial_object, reset_angle)
-	    self.parameters_as.set_succeeded(TestParametersResult(result=0), text="SUCCESS")
-        except:
-	    self.parameters_as.set_aborted(TestParametersResult(result=100), text="FAILED")
+            self.hardware.action_caller(trial_object, reset_angle)
+            self.parameters_as.set_succeeded(TestParametersResult(result=0), text="SUCCESS")
+        except Exception as e:
+            print("failed set_test_parameters: " + str(e))
+            self.parameters_as.set_aborted(TestParametersResult(result=100), text="FAILED")
 
     def reset_callback(self, goal):
         try:
-	    self.reset_as.publish_feedback(StageFeedback(status="Resetting Drawer"))
+            print("reset_hardware")
+            self.reset_as.publish_feedback(StageFeedback(status="Resetting Drawer"))
             self.hardware.testbed_reset()
-	    self.reset_as.set_succeeded(StageResult(result=0), text="SUCCESS")
-        except:
-	    self.reset_as.set_aborted(StageResult(result=100), text="FAILED")
+            self.reset_as.set_succeeded(StageResult(result=0), text="SUCCESS")
+        except Exception as e:
+            print("failed reset_hardware: " + str(e))
+            self.reset_as.set_aborted(StageResult(result=100), text="FAILED")
 
-
-def cleanup_wrapper():
-    gpio.cleanup()
+    def cleanup_wrapper():
+        gpio.cleanup()
 
 if __name__ == "__main__":
     #initialize hardware
